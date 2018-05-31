0ba14a4cdc33ab0ed2786be8b8adda666b9740e5
SyRedding <syeugene.redding@ucsf.edu>
2018-05-31 10:31:50 -0700

    working on making test images openable

--- testdata/__init__.py
+++ testdata/__init__.py
@@ -1,8 +1,7 @@
 from skimage.external.tifffile import imread
-import os, sys
-sys.path.insert(0, os.path.abspath(".."))
+pa =testdata.__path__
 
-print(os.listdir("."))
+print(os.listdir(pa))
 test_maximum = imread("testdata/single_max.tif")
 test_image = imread("1_align.tif")
 #test_images = [imread(i) for i in os.listdir('.') if i.endswith("_align.tif")]
