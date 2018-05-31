c5abbcebb9f790fc8afcdd7844f91a622d183bec
SyRedding <syeugene.redding@ucsf.edu>
2018-05-31 10:11:26 -0700

    added test images

--- testdata/__init__.py
+++ testdata/__init__.py
@@ -1,3 +1,7 @@
+from skimage.external.tifffile import imread
+import os
 
 
-
+test_maximum = imread("single_max.tif")
+test_image = imread("1_align.tif")
+test_images = [imread(i) for i in os.listdir('.') if i.endswith("_align.tif")]
