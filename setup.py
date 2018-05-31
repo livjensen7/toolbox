ff6ea4a762acda88c7118ec09f25c1c6c8fbf6f2
SyRedding <syeugene.redding@ucsf.edu>
2018-05-31 14:48:24 -0700

    fix

--- setup.py
+++ setup.py
@@ -12,9 +12,6 @@ setuptools.setup(
     long_description_content_type="text/markdown",
     url="https://github.com/github/ReddingLab/toolbox",
     packages=setuptools.find_packages(),
-    package_data={'toolbox/testdata': ['1_align.tif', '2_align.tif', '3_align.tif', '4_align.tif', '5_align.tif', '6_align.tif',
-                              '7_align.tif', '8_align.tif', '9_align.tif', '10_align.tif', '11_align.tif']},
-
     include_package_data=True,
     install_requires=[
           'numpy','scipy','scikit-image'
