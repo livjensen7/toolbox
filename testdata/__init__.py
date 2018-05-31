21f84a953688bac1596803cb67c550d7faaff4f1
SyRedding <syeugene.redding@ucsf.edu>
2018-05-31 10:49:01 -0700

    working on making test images openable

--- testdata/__init__.py
+++ testdata/__init__.py
@@ -1,3 +1,11 @@
 import os
+from skimage.external.tifffile import imread
+
+
 dir_path = os.path.dirname(os.path.realpath(__file__))
 print(dir_path)
+
+
+
+def single_max():
+    return imread(dir_path+"/single_max.tif")
