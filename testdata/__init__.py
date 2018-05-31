266d569354cdcc1b63e4ddc18de5f9083092c9e9
SyRedding <syeugene.redding@ucsf.edu>
2018-05-31 10:56:03 -0700

    working on making test images openable

--- testdata/__init__.py
+++ testdata/__init__.py
@@ -1,14 +1,11 @@
 import os
-from skimage.external.tifffile import imread
+import skimage.external.tifffile as ski
 
 
 dir_path = os.path.dirname(os.path.realpath(__file__))
-print(dir_path)
-
-
 
 def single_max():
-    return imread(dir_path+"/single_max.tif")
+    return ski.imread(dir_path+"/single_max.tif")
 
 def image_stack():
-    return [imread(i) for i in os.listdir("dir_path") if i.endswith(_align.tif)]
+    return [ski.imread(i) for i in os.listdir(dir_path) if i.endswith(_align.tif)]
